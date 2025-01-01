80149254cafb219b9450a7e5c397db1a5bf0422f
Fabrizio Montanez <AndreFabrizioMP10@hotmail.com>
2025-01-01 11:41:04 -0500

    Sales Project - N°006

---
+++ Sales_System(Coffee)/src/Controller/Methods.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Fabrizio
+ */
+public interface Methods {
+
+    public abstract void RegistrarPedido();
+
+    public abstract void MostrarListado();
+
+    public abstract void EliminarPedido();
+
+    public abstract void ActualizarPedido();
+}
--- Sales_System(Coffee)/src/Model/Cliente.java
+++ Sales_System(Coffee)/src/Model/Cliente.java
@@ -9,5 +9,16 @@ package Model;
  * @author Fabrizio
  */
 public class Cliente {
+    //Atributos
+    public int id_cli;
+    public String nombre_cli;
+    public String apellido_cli;
+    public String direccion_principal_cli;
+    public String direccion_secundaria_cli;
+    public int telefono_principal_cli;
+    public int telefono_secundario_cli;
+    public String correoElectronico_cli;
+    
+    //Metodos
     
 }
